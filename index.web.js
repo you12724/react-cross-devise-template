8edf78e1bf45cd89094c0dd67d720e83d36f7289
you12724 <you12724@gmail.com>
2017-11-16 02:25:04 +0900

    androidでも動作確認（ただindex.jsしか反応しない問題。今回は見送り）

---
+++ index.web.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+import App from './src/views/web/routes';
+
+render(
+  App,
+  document.getElementById('app')
+);
--- src/views/web/routes.js
+++ src/views/web/routes.js
@@ -1,14 +1,13 @@
 import React from 'react';
-import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 
 import store from '../store';
 import Root from '../views/web/containers/Root/Root';
 
-
-render(
+const App = () => (
   <Provider store={store}>
     <Root />
-  </Provider>,
-  document.getElementById('app')
+  </Provider>
 );
+
+export default App;
